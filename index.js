bf32f346148fe2b6150025b1a8fecfb99c6e9e94
Edgar Mikuro <edgarmikuro@gmail.com>
2025-03-31 13:54:16 +0300

    Completes phase-1 review strings lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Edgar Mikuro';
+
+const welcomeMessage = 'Welcome to Flatbook, ' + 'Edgar Mikuro!';
+
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + 'EDGAR MIKURO!';
+
+const shortGreeting = 'Welcome, ' + 'E!';
